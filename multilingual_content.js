85aa2d2e61c2ae7f6c72083c631f74281c77ebcd
Josef Haeusel <josef.haeusel@yahoo.de>
2024-07-16 16:40:14 +0200

    Language Correction

--- multilingual_content.js
+++ multilingual_content.js
@@ -3,8 +3,8 @@ const I18n = VueI18n.createI18n({
     messages: {
         English: {
             title: {
-                main: 'Soundlogo Wizard',
-                sub: 'Add the iconic Telekom Soundlogo at the end of all videos.<br>Keep everything on brand with the help of AI.'
+                main: 'Sound Logo Wizard',
+                sub: 'Add the iconic Telekom Sound Logo at the end of all videos.<br>Keep everything on brand with the help of AI.'
             },
             upload: {
                 instructions: 'Upload a Video or Drop the File Here',
@@ -25,7 +25,7 @@ const I18n = VueI18n.createI18n({
                 result: 'Download Result'
             },
             soundlogo: {
-                key: 'Soundlogo Key',
+                key: 'Sound Logo Key',
                 recommended: 'Recommended',
                 bothKeysPossible: 'Both possible'
             },
@@ -35,11 +35,11 @@ const I18n = VueI18n.createI18n({
                 logoDetected: 'Detected Animation',
                 appendedDescription: 'Because the T-Outro Animation could not be detected, it was appended to the video\'s ending.',
                 detectedDescription: 'The T-Outro Animation was detected by Computer-Vision AI.',
-                setSoundlogo: 'Set Soundlogo',
-                synchronized: 'The Telekom Soundlogo has been synchronized to the T-Outro animation.',
+                setSoundlogo: 'Set Sound Logo',
+                synchronized: 'The Telekom Sound Logo has been synchronized to the T-Outro animation.',
                 matched: 'Its tonality and loudness have been matched to the AI-detected music. The music has been filtered and faded for a smooth transition.',
-                audioEmpty: 'Your video\'s audio track is empty or doesn\'t exist. The Soundlogo\'s tonality and loudness have been set to standardized values.',
-                silentEnding: 'Your video ending (except animation) is silent. The Soundlogo\'s tonality has been set to standardized values.',
+                audioEmpty: 'Your video\'s audio track is empty or doesn\'t exist. The Sound Logo\'s tonality and loudness have been set to standardized values.',
+                silentEnding: 'Your video ending (except animation) is silent. The Sound Logo\'s tonality has been set to standardized values.',
                 convertedFormat: 'Converted Format',
                 convertedDescription: 'Converted video codec from {codec} to H264 (.mp4).',
                 masteredLoudness: 'Mastered Loudness',
@@ -48,8 +48,8 @@ const I18n = VueI18n.createI18n({
         },
         Deutsch: {
             title: {
-                main: 'Soundlogo Wizard',
-                sub: 'Binden Sie das ikonische Telekom Soundlogo an das Ende aller Videos an.<br>Bleibe mithilfe von KI on-Brand.'
+                main: 'Sound Logo Wizard',
+                sub: 'Binden Sie das ikonische Telekom Sound Logo an das Ende aller Videos an.<br>Bleibe mithilfe von KI on-Brand.'
             },
             upload: {
                 instructions: 'Laden Sie ein Video hoch oder ziehen Sie die Datei hierher',
@@ -70,7 +70,7 @@ const I18n = VueI18n.createI18n({
                 result: 'Video Herunterladen'
             },
             soundlogo: {
-                key: 'Soundlogo Tonart',
+                key: 'Sound Logo Tonart',
                 recommended: 'Empfohlen',
                 bothKeysPossible: 'Beide Möglich'
             },
@@ -80,11 +80,11 @@ const I18n = VueI18n.createI18n({
                 logoDetected: 'Animation erkannt',
                 appendedDescription: 'Da die T-Outro Animation nicht erkannt werden konnte, wurde sie an das Videoende angehängt.',
                 detectedDescription: 'Die T-Outro Animation wurde durch Computer-Vision AI erkannt.',
-                setSoundlogo: 'Soundlogo platziert',
-                synchronized: 'Das Telekom Soundlogo wurde mit der T-Outro Animation synchronisiert.',
+                setSoundlogo: 'Sound Logo platziert',
+                synchronized: 'Das Telekom Sound Logo wurde mit der T-Outro Animation synchronisiert.',
                 matched: 'Seine Tonalität und Lautstärke wurden an die von der KI erkannte Musik angepasst. Die Musik wurde gefiltert und für einen sanften Übergang ausgeblendet.',
-                audioEmpty: 'Die Audiospur Ihres Videos ist leer oder existiert nicht. Die Tonalität und Lautstärke des Soundlogos wurden auf standardisierte Werte eingestellt.',
-                silentEnding: 'Ihr Videoende (außer Animation) ist still. Die Tonalität des Soundlogos wurde auf standardisierte Werte eingestellt.',
+                audioEmpty: 'Die Audiospur Ihres Videos ist leer oder existiert nicht. Die Tonalität und Lautstärke des Sound Logos wurden auf standardisierte Werte eingestellt.',
+                silentEnding: 'Ihr Videoende (außer Animation) ist still. Die Tonalität des Sound Logos wurde auf standardisierte Werte eingestellt.',
                 convertedFormat: 'Format konvertiert',
                 convertedDescription: 'Videocodec konvertiert von {codec} zu H264 (.mp4).',
                 masteredLoudness: 'Lautstärke gemastered',
